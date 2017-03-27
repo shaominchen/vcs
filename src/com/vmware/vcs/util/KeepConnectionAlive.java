93c0cdff078dcbfd414342dc7fbe8930fc3696f1
Shaomin Chen <shchen@shchen-m02.vmware.com>
2017-03-27 10:37:21 -0700

    Basic VCS implementation as an VC Extension
    The Basic VCS implementaion performs the foolowing actions:
    1. Register itself as a VC extension
    2. Integrate with VC Solution Manager
    3. Use OVF to deploy an ESX Agent VM
    4. Deploy a VIB on the ESX host

---
+++ src/com/vmware/vcs/util/KeepConnectionAlive.java
@@ -0,0 +1,52 @@
+/* **********************************************************
+ * Copyright 2017 VMware, Inc.  All rights reserved.
+ *      -- VMware Confidential
+ * **********************************************************/
+
+package com.vmware.vcs.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class KeepConnectionAlive extends Thread {
+
+   private static Log _logger = LogFactory.getLog(KeepConnectionAlive.class);
+
+   private final VimConnection _vimConnection;
+   private final EamConnection _eamConnection;
+
+   public KeepConnectionAlive(VimConnection vimConnection, EamConnection eamConnection) {
+      _vimConnection = vimConnection;
+      _eamConnection = eamConnection;
+      setDaemon(true);
+   }
+
+   @Override
+   public void run() {
+      while (true) {
+         if (_vimConnection.isConnected()) {
+            // Invoke method to keep the connection alive.
+            try {
+               _vimConnection.ensureConnection();
+            } catch (Exception e) {
+               _logger.warn("No VIM connection: " + e.getMessage());
+            }
+         }
+         if (_eamConnection.isConnected()) {
+            // Invoke method to keep the connection alive.
+            try {
+               _eamConnection.getStub().queryAgency(_eamConnection.getEsxAgentManager());
+            } catch (Exception e) {
+               _logger.warn("No EAM connection: " + e.getMessage());
+            }
+         }
+
+         // Sleep five minutes
+         try {
+            Thread.sleep(5 * 60 * 1000);
+         } catch (InterruptedException e) {
+         }
+      }
+   }
+
+}
